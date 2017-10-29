c7ebb9a9f90870d8209fa6ef80b1979db5750acc
codelover <626973633@qq.com>
2017-10-29 18:59:19 +0800

    测试链表

--- test_case/test_cl_link.c
+++ test_case/test_cl_link.c
@@ -10,26 +10,39 @@ typedef struct test{
 
 void* handler(void *arg)
 {
+    test* t = cl_link_get_data(arg, test, node);
+    printf("%p\n",t );
+    free(t);
+
     int* r = malloc(sizeof(int));
     *r = rand()%10;
-    printf("%d\n", cl_link_get_data(arg, test, node)->n);
+    // printf("%d\n", cl_link_get_data(arg, test, node)->n);
     return r;
+}
+int condition(void* node, void* key){
+    test* t = cl_link_get_data(node, test, node);
+    if(t->n == (int)key)
+        return CANFIND;
+    else
+        return NOTFIND;
 }
 int main(){
     void* res[10];
     cl_link* link = cl_link_create();
     for(int i=0; i<10; i++){
         test* a = malloc(sizeof(test));
+        printf("%p\n",a );
         a->n = i;
         cl_link_add_back(link, cl_link_get_node(a,test,node));
 
-        if( i == 9){
-            cl_link_get_front(link);
-        }
+        // if( i == 9){
+        //     cl_link_get_front(link);
+        // }
     }
+
+    test* t = cl_link_find(link, (void *)5, condition);
+    if(t != NULL)
+    printf("%d\n", cl_link_get_data(t,test,node)->n);
     cl_link_each(link, res, handler);
-    // for(int i=0; i<10; i++){
-    //     printf("%d\n", *(int*)(res[i]));
-    // }
     return 0;
 }
