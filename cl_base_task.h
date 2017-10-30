229d4668ef1889dfde4ae45a53d5572ce1cf0c29
codelover <626973633@qq.com>
2017-10-30 11:14:38 +0800

    任务对象

--- cl_base_task.h
+++ cl_base_task.h
@@ -3,13 +3,19 @@
 
 #include "cl_link.h"
 
-typedef struct cl_task{
+typedef struct cl_base_task{
     cl_link_node    cl_task_node;               //任务队列需要
     void*           self;                       //任务回调参数
     void*           (*handler)(void* self);     //任务回调函数
     void*           (*destroy)(void* self);     //任务销毁函数
     int             need_destroy;               //是否需要销毁
-}cl_task;
+}cl_base_task;
 
 
+#define cl_base_task_get_base(aim, type, node)       \
+    (cl_base_task *) ((u_char *) aim + offsetof(type, node))
+
+
+#define cl_base_task_get_task(aim, type, node)      \
+    (type *) ((u_char *) aim - offsetof(type, node))
 #endif
