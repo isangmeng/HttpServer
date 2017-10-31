40c1491ffd284f15030feb5bdd58ac4e0ba1d432
codelover <626973633@qq.com>
2017-11-01 01:12:30 +0800

    主函数

--- main.c
+++ main.c
@@ -10,9 +10,9 @@ typedef struct manage{
 
 void* showf(void* arg)
 {
-    cl_base_task* cb = arg;
-    cl_http_event* ht = cb->self;
-    printf("%d,%d\n", ht->client->fd,ht->event_node.status);
+    // cl_base_task* cb = arg;
+    // cl_http_event* ht = cb->self;
+    // printf("%d,%d\n", ht->client->fd,ht->event_node.status);
     return NULL;
 }
 
@@ -23,7 +23,7 @@ void* show(void* arg)
     {
         // m->pool
         printf("%d\n", m->pool->finish_task->sum);
-        cl_link_each(m->pool->finish_task, NULL, showf);
+        // cl_link_each(m->pool->finish_task, NULL, showf);
         sleep(2);
     }
 
@@ -39,7 +39,7 @@ int main(int argc, char* argv[])
     m->accept_task.handler = show;
     cl_pthread_pool_add_task(pool, cl_base_task_get_base(m, manage, accept_task));
     cl_event* event = cl_event_create(100, 100, pool);
-    cl_accept_task* accept =  cl_accpet_task_create("127.0.0.1", "8888", 10, pool, event);
+    cl_accept_task* accept =  cl_accpet_task_create("127.0.0.1", "7777", 10, pool, event);
     cl_pthread_pool_add_task(pool, cl_base_task_get_base(accept, cl_accept_task, accept_task));
     cl_event_wait_event(event);
     while(1);
