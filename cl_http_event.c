5ba44fd1a9a6f8c5ced33b372ccf65e4ef614d9b
codelover <626973633@qq.com>
2017-11-01 01:11:53 +0800

    处理http

--- cl_http_event.c
+++ cl_http_event.c
@@ -4,7 +4,7 @@
 
 cl_http_event* cl_http_event_create(cl_socket* client, cl_event* event)
 {
-    printf("malloc base task index\n");
+    // printf("malloc base task index\n");
     cl_http_event* http_task = malloc(sizeof(cl_http_event));
     http_task->client = client;
     // http_task->task.self = http_task;
@@ -24,7 +24,7 @@ cl_http_event* cl_http_event_create(cl_socket* client, cl_event* event)
 
 void* handler(void* arg)
 {
-    printf("malloc base task handler\n");
+    // printf("malloc base task handler\n");
     cl_http_event* http_task = arg;
     http_task->event_node.events = EPOLLOUT;
     // http_task->task.handler = response;
@@ -36,31 +36,33 @@ void* handler(void* arg)
         int n = cl_socket_read(http_task->client);
         // if(n <= 0)
         //     break;
-        printf("recv data,%s\n",http_task->client->recv_buf);
+        // printf("recv data,%s\n",http_task->client->recv_buf);
     // }
-    sleep(4);
+    // sleep(4);
     if(n > 0)
         cl_event_add_event(http_task->event, &(http_task->event_node));
     else
-        shutdown(http_task->client->fd, SHUT_RDWR);
+        close(http_task->event_node.fd);
+    // close(http_task->event_node.fd);
     return NULL;
 }
 void* response(void* arg)
 {
-    printf("malloc base task response\n");
+    // printf("malloc base task response\n");
     cl_http_event* http_task = arg;
     http_task->event_node.events = EPOLLIN;
     // http_task->task.handler = handler;
     http_task->event_node.task = malloc(sizeof(cl_base_task));
     ((cl_base_task*)(http_task->event_node.task))->self = http_task;
     ((cl_base_task*)(http_task->event_node.task))->handler = handler;
-    sprintf(http_task->client->send_buf, "HTTP/1.0 200 OK\r\nServer:Codelover\r\nConnection: Close\r\nDate:Mon,6Oct2003 13:23:42 GMT\r\nContent-Length:30\r\n\r\n<h1>codelover http server</h1>");
+    sprintf(http_task->client->send_buf, "HTTP/1.0 200 OK\r\nServer: Codelover\r\nConnection: Close\r\nDate:Mon,6Oct2003 13:23:42 GMT\r\nContent-Length:30\r\n\r\n<h1>codelover http server</h1>");
     // strcpy(http_task->client->send_buf, "fuck you");
     int n = cl_socket_write(http_task->client);
-    sleep(5);
+    // sleep(5);
     if(n > 0)
         cl_event_add_event(http_task->event, &(http_task->event_node));
     else
-        shutdown(http_task->client->fd, SHUT_RDWR);
+        close(http_task->event_node.fd);
+    close(http_task->event_node.fd);
     return NULL;
 }
